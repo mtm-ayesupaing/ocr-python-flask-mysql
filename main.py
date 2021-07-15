cc09099062570c92bfdd5bc19438bb7b774aed0b
Tin Myat Win <scm.dott.developer6@gmail.com>
2021-07-15 14:59:08 +0630

    set main.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import os
 import pymysql
 from flask_bcrypt import Bcrypt
-from ocr_core import ocr_core
 from app import app
 from db import mysql
-from flask import jsonify
-from flask import flash, request
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 from werkzeug.security import generate_password_hash, check_password_hash
+from tesserocr import PyTessBaseAPI, RIL, iterate_level
+from PIL import Image
+import re
+import os
 
 bcrypt = Bcrypt(app)
-
 UPLOAD_FOLDER = '/uploads/'
 		
 @app.route('/add', methods=['POST'])
@@ -274,6 +276,69 @@ def delete_passport(id):
 	finally:
 		cursor.close() 
 		conn.close()
+
+@app.route('/ocr', methods=['GET', 'POST'])
+def processFile():
+    try:
+        if request.method == 'POST':
+            file = request.files['file']
+            if file:
+                data = []
+                result = {}
+                file.save(os.path.join(os.getcwd() + UPLOAD_FOLDER, file.filename))
+                images = ['uploads/' + file.filename]
+                resp = jsonify('Image uploaded successfully!')
+                with PyTessBaseAPI(path='./tessdata/.', lang='eng') as api:    
+                    for img in images:
+                        api.SetImageFile(img)
+                        result['raw_data'] = api.GetUTF8Text()
+                        data = api.GetUTF8Text().split('\n')
+                        data = [item for item in data if item != '' and item != ' ' and item != '  ']
+                    
+                        for i, value in enumerate(data):
+                            if i == 0:
+                                result['title'] = value                  
+                            if re.search('Name', value):
+                                nameVal = data[i + 1]
+                                result['name'] = nameVal
+                            elif re.search('Nation', value):
+                                nationVal = data[i + 1]
+                                result['nation'] = nationVal
+                            elif re.search('Date of birth', value):
+                                dobVal = data[i + 1]
+                                result['dob'] = dobVal
+                            elif re.search('Sex', value):
+                                genderVal = data[i + 1]
+                                result['gender'] = genderVal
+                            elif re.search('Date of issue', value):
+                                dateIssueVal = data[i + 1]
+                                result['issue_date'] = dateIssueVal
+                            elif re.search('Date of expire', value):
+                                dateExpireVal = data[i + 1]
+                                result['expire_date'] = dateExpireVal
+                            elif re.search('Place of', value):
+                                pobVal = data[i + 1]
+                                result['pob'] = pobVal
+                            elif re.search('Authority', value):
+                                authVal = data[i + 1]
+                                result['authority'] = authVal
+                            elif re.search('Passport No', value):
+                                passportNoVal = data[i + 1]
+                                result['passport_no'] = passportNoVal
+                            elif re.search('type', value):
+                                passportTypeVal = data[i + 1]
+                                result['passport_type'] = passportTypeVal
+                            elif re.search('Country Code', value):
+                                countryCodeVal = data[i + 1]
+                                result['country_code'] = countryCodeVal
+
+                resp = jsonify(result)
+                resp.status_code = 200
+                return resp
+            else :
+                return not_found()
+    except Exception as e:
+            print(e)
 		
 @app.errorhandler(404)
 def not_found(error=None):
